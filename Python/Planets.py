e1121da2d6c91d281b498d3d40152bb84e33d010
arista <atd-help@arista.com>
2025-03-02 14:55:40 +0000

    python lab completion

---
+++ Python/Planets.py
@@ -0,0 +1,16 @@
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+for item in planets:
+    print(item)
+
+obvious = { 'water': 'wet', 'fire': 'hot', 'sky': 'blue'}
+
+print(obvious['fire'])
+
+for item in obvious:
+    print(obvious[item])
+
+for item in obvious.values():
+    print(item)
+
+    
---
+++ Python/string.py
@@ -0,0 +1,6 @@
+st1 = "Hello"
+st2 = "world!"
+print(f"{st1} {st2}")
+
+
+git 
